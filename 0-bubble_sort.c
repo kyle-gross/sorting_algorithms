107be0ca5fa1d1c116952d586962e2f6112b0c50
Kyle <kyg555@outlook.com>
2021-08-17 12:28:49 -0700

    Add task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ * swap - swaps two integers within an array
+ * @xp: x position
+ * @yp: y position
+ * Return: void
+ */
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+
+    *xp = *yp;
+    *yp = temp;
+}
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * using the Bubble Sort algorithm
+ * @array: int - array
+ * @size: size_t - size of array
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j;
+
+    for (i = 0; i < (size - 1); i++)
+    {
+        for (j = 0; j < (size - i - 1); j++)
+        {
+            if (array[j] > array[j + 1])
+                swap(&array[j], &array[j + 1]);
+            print_array(array, size);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# sorting_algorithms
+# Sorting Algorithms
+
+Important topics:
+* Big O notation
+* Sorting algorithms
+    * Bubble sort
+    * Insertion sort
+    * Selection sort
+    * Quick sort
+
+Resources:
+* [Sorting algorithm](https://en.wikipedia.org/wiki/Sorting_algorithm "Sorting algorithm")
+    * [Sorting algorithm animations](https://www.toptal.com/developers/sorting-algorithms "Sorting algorithm animations")
+* [Big O Notation](https://stackoverflow.com/questions/487258/what-is-a-plain-english-explanation-of-big-o-notation "Big O Notation")
