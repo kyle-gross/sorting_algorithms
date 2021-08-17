fa08bc2eaa9759db6870488d2fffa78bb006d918
Kyle <kyg555@outlook.com>
2021-08-17 12:34:46 -0700

    Fix file to compile

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,17 +23,17 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	if (!array)
-		return
-
-	for (i = 0; i < (size - 1); i++)
+	if (array)
 	{
-		for (j = 0; j < (size - i - 1); j++)
+		for (i = 0; i < (size - 1); i++)
 		{
-			if (array[j] > array[j + 1])
+			for (j = 0; j < (size - i - 1); j++)
 			{
-				swap(&array[j], &array[j + 1]);
-				print_array(array, size);
+				if (array[j] > array[j + 1])
+				{
+					swap(&array[j], &array[j + 1]);
+					print_array(array, size);
+				}
 			}
 		}
 	}
