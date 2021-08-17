a3358b4d90bbd13cf36d6c31e2bf17ededc287a9
Kyle <kyg555@outlook.com>
2021-08-17 12:32:41 -0700

    Fix betty and add null check

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,10 +7,10 @@
  */
 void swap(int *xp, int *yp)
 {
-    int temp = *xp;
+	int temp = *xp;
 
-    *xp = *yp;
-    *yp = temp;
+	*xp = *yp;
+	*yp = temp;
 }
 /**
  * bubble_sort - sorts an array of integers in ascending order
@@ -21,15 +21,20 @@ void swap(int *xp, int *yp)
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
+	size_t i, j;
 
-    for (i = 0; i < (size - 1); i++)
-    {
-        for (j = 0; j < (size - i - 1); j++)
-        {
-            if (array[j] > array[j + 1])
-                swap(&array[j], &array[j + 1]);
-            print_array(array, size);
-        }
-    }
+	if (!array)
+		return
+
+	for (i = 0; i < (size - 1); i++)
+	{
+		for (j = 0; j < (size - i - 1); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
 }
